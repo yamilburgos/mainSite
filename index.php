e6068242325970facdeab5d8c96f2292d3ba22a1
Yamil Burgos <yamil.burgos1293@gmail.com>
2017-08-23 14:35:58 -0400

    Working on blog section for main portfolio site

--- index.php
+++ index.php
@@ -5,25 +5,18 @@
     $from = 'From: Demo'; 
     $to = 'yamil.burgos1293@gmail.com'; 
     $subject = 'Hello';
-    $human = $_POST['human'];
 			
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 				
     if ($_POST['submit']) {
         if ($name != '' && $email != '') {
-            if ($human == '4') {
-                if (mail ($to, $subject, $body, $from)) {
-                    echo '<p>Your message has been sent!</p>';
-                }
-            
-                else {
-                    echo '<p>Something went wrong, go back and try again!</p>';
-                }
-            }
-
-            else if ($_POST['submit'] && $human != '4') {
-                echo '<p>You answered the anti-spam question incorrectly!</p>';
-            }
+			if (mail ($to, $subject, $body, $from)) {
+				echo '<p>Your message has been sent!</p>';
+			}
+			
+			else {            
+				echo '<p>Something went wrong, go back and try again!</p>';                
+			}        
         } 
         
         else {
